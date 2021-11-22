6526d82058843e016766eb17f0403e435ff0a49e
jalilmanuel25 <jalilmanuel25@gmail.com>
2021-11-22 17:25:43 -0600

    se maquetan las dos versiones del menu

--- js/main.js
+++ js/main.js
@@ -1,3 +1,29 @@
 window.onscroll = function() {
     console.log( pageYOffset );
+
+
+
 }
+
+{/* 
+<ul class="bar-menu f-chr-bold blue-1">
+    <div class="bar-menu-container container">
+        <li>
+            <a href="#">Soy cliente</a>
+        </li>
+        <li>
+            <img src="img/LOGO PRINCIPAL_AUARE.png" alt="logo" class="logo">
+        </li>
+        <li>
+            <a href="#">Menú</a>
+        </li>
+    </div>
+</ul> 
+
+<div class="bar-menu shadow">
+    <div class="bar-menu-container bar-menu-container-2 container">
+            <img src="img/LOGO PRINCIPAL_AUARE.png" alt="logo" class="logo">
+            <img class="menu-icon" src="img/MENU1_AUARE.png" alt="menu">
+    </div>
+</div>
+*/}
